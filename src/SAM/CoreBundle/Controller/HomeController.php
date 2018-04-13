d61e1a974b35dcc9cf59ee16049a25cedd7fd77d
Samakunchan <samakunchan@gmail.com>
2018-04-13 22:56:39 +0200

    Some pages
    - refactor pages with layout
    - Add page home + link
    - Add page contact + link
    - Add page for FOSUser (1/2) + link

---
+++ src/SAM/CoreBundle/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samakunchan
+ * Date: 13/04/2018
+ * Time: 22:40
+ */
+
+namespace SAM\CoreBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('SAMCoreBundle:Core:index.html.twig');
+    }
+
+    public function contactAction()
+    {
+        return $this->render('SAMCoreBundle:Core:contact.html.twig');
+    }
+}
