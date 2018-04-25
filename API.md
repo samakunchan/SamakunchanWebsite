04e1af67f5cdea85219fcab5055de6b736f030f3
Samakunchan <samakunchan@gmail.com>
2018-04-25 10:06:05 +0200

    API github
    - API github connected
    - Read last commit in home page
    - Create API.md
    - Create ajax.js for using GET request
    - Using <pre> tags for parse the commit

---
+++ API.md
@@ -0,0 +1,10 @@
+API REST
+
+Pré-requis:
+--
+
+    Utiliser curl dans l'invite de commande
+
+Exemple de connection à l'API github
+--
+    curl https://api.github.com/users/samakunchan
---
+++ src/SAM/CoreBundle/Resources/public/js/ajax.js
@@ -0,0 +1,52 @@
+var Ajax = {
+    ajaxGet : function (url, callback) {
+        var req = new XMLHttpRequest();
+        req.open("GET", url);
+        req.addEventListener("load", function () {
+            if (req.status >= 200 && req.status < 400) {
+                callback(req.responseText);
+            } else {
+                console.error(req.status + " " + req.statusText + " " + url);
+            }
+        });
+        req.addEventListener("error", function () {
+            console.error("Erreur réseau avec l'URL " + url);
+        });
+        req.send(null);
+    },
+    ajaxPost : function (url, data, callback, isJson) {
+        var req = new XMLHttpRequest();
+        req.open("POST", url);
+        req.addEventListener("load", function () {
+            if (req.status >= 200 && req.status < 400) {
+                callback(req.responseText);
+            } else {
+                console.error(req.status + " " + req.statusText + " " + url);
+            }
+        });
+        req.addEventListener("error", function () {
+            console.error("Erreur réseau avec l'URL " + url);
+        });
+        if (isJson) {
+            req.setRequestHeader("Content-Type", "application/json");
+            data = JSON.stringify(data);
+        }
+        req.send(data);
+    }
+};
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+
